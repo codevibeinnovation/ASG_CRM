bf59e7fb9d7d5d60d8bc71b210aa22b9d6bec53d
hinal010 <hinalprajapati010@example.com>
2026-05-22 13:14:53 +0530

    change login

--- auth.py
+++ auth.py
@@ -3,12 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CLIENT_ID=os.getenv("CLIENT_ID")
-
-CLIENT_SECRET=os.getenv("CLIENT_SECRET")
-
-GOOGLE_REDIRECT_URI=os.getenv("GOOGLE_REDIRECT_URI")
-
 SECRET_KEY=os.getenv("SECRET_KEY")
 
 ALGORITHM=os.getenv("ALGORITHM")
